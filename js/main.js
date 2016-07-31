885734fbeb499e847c5ec54083bacb6a6fcc7e94
lidiia.petrova <lidiia.petrova@billing.ru>
2016-07-31 20:35:51 +0300

    School

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 $(function(){
-	
+	$('.filter-block .form-select').selectpicker();
+	$('.filter-switch').click(function(){
+		$('.filter-switch').toggleClass('on');
+		$('.main-filter').slideToggle("slow");
+	})
 })
