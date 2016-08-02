bec56f147d6d217eb9bd418a76f71ffa01ef420e
lidiia.petrova <lidiia.petrova@billing.ru>
2016-08-02 23:28:21 +0300

    Detail end

--- js/main.js
+++ js/main.js
@@ -3,5 +3,12 @@ $(function(){
 	$('.filter-switch').click(function(){
 		$('.filter-switch').toggleClass('on');
 		$('.main-filter').slideToggle("slow");
+	});
+	$('.left-nav span').click(function(){
+		$('.left-nav span.current').removeClass('current');
+		$(this).addClass('current');
+		var id = $(this).attr('data-href');
+		$('.right-tab.current-tab').removeClass('current-tab');
+		$(id).addClass('current-tab');
 	})
 })
